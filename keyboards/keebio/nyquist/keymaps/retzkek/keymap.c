aa2447fcd314642b62e4681760d841e50a07b158
Kevin Retzke <retzkek@gmail.com>
2021-04-29 10:41:13 -0500

    fix ctrl, move volume keys so they're not on mods

--- keyboards/keebio/nyquist/keymaps/retzkek/keymap.c
+++ keyboards/keebio/nyquist/keymaps/retzkek/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT( \
   KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  KC_CTRL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
   TG(_FN),  KC_LALT, KC_TAB,  KC_LCTL, KC_LGUI, KC_BSPC, KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL, KC_ENT \
 ),
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_FN] = LAYOUT( \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-  KC_VOLU, KC_GRV , KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP, KC_7,    KC_8,    KC_9,    KC_MINS, KC_EQUAL, \
-  KC_VOLD, KC_LPRN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_RPRN, KC_PGDN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_EQUAL, \
-  KC_MUTE, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, _______, KC_1,    KC_2,    KC_3,    KC_ASTR, _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+  _______, KC_GRV , KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_PGUP, KC_7,    KC_8,    KC_9,    KC_MINS, KC_EQUAL, \
+  _______, KC_LPRN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_RPRN, KC_PGDN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_EQUAL, \
+  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_VOLD, _______, KC_1,    KC_2,    KC_3,    KC_ASTR, _______, \
   TG(_AD), _______, _______, _______, _______, _______, _______, _______, KC_DOT,  KC_0,    _______, _______ \
 ),
 
